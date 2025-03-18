4c1f11bcf5f6bddf4919861f60f95c7f628b7720
SubaruImpreza22B <00001096561785sp@aluno.educacao.sp.gov.br>
2025-03-18 07:10:04 -0300

    atualização

--- Aula20.py
+++ Aula20.py
@@ -9,3 +9,14 @@ print(f"O número removido será: {ListaImpares[3]}")
 
 ListaImpares.remove(7)
 print(ListaImpares)
+
+Fibonacci = [8, 1, 0, 5, 13, 1, 3, 2]
+Fibonacci.sort()
+print(Fibonacci)
+
+Pi = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+
+print(Pi[5])
+print(max(Pi))
+print(min(Pi))
+print(Pi[3:5])
